b5405046c3a7c9cb205c24d93b6f8a0e6c9d354f
azka13labib-ops <azka13labib@gmail.com>
2026-07-11 23:14:37 +0700

    Scaffold: Generate Models, Controllers, Middleware, and Commands for backend

---
+++ santricard-be/app/Console/Commands/ResetLimitHarian.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+
+#[Signature('app:reset-limit-harian')]
+#[Description('Command description')]
+class ResetLimitHarian extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
